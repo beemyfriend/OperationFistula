523bcdb5d673d46663d81e8f8833a8f1a846ec21
beemyfriend <plzbeemyfriend@gmail.com>
2020-03-08 21:32:32 -0400

    added app and diffDta

--- OFApp/app.R
+++ OFApp/app.R
@@ -13,6 +13,8 @@ library(tidyverse)
 dta <- readr::read_csv('../data/YearlyAttitudeTowardsWifeBeating.csv') %>%
   dplyr::mutate(Indicator = stringr::str_remove(Indicator, '^if she |^for '))
 
+diffDta <- readr::read_csv('../data/DiffYearAttitudeTowardsWifeBeating.csv')
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -22,23 +24,43 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins
    sidebarLayout(
       sidebarPanel(
+        h3('Control for all'),
+        selectInput('dem',
+                    'Choose Demographic Key',
+                    unique(dta$`Characteristic Category`)),
+        hr(),
+        h3('Control for trends'),
         selectInput('country',
                     'Choose country of interest',
                     unique(dta$`Country Name`)),
-        selectInput('dem',
-                    'Choose Demographic Key',
-                    unique(dta$`Characteristic Category`))
+
+        hr(),
+        h3('Control for delta'),
+        selectInput('indicator',
+                    'Choose Indicator',
+                    unique(diffDta$Indicator)),
+        selectInput('lab',
+                    'Choose Demographic Section',
+                    unique(dplyr::filter(diffDta, `Characteristic Category` == 'Education')$`Characteristic Label`)),
+        selectInput('gender',
+                    'Choose Gender',
+                    unique(diffDta$gender))
       ),
 
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("countryPlot")
+        h3('Trends'),
+         plotOutput("countryPlot"),
+        br(),
+        hr(),
+        h3('Delta'),
+         plotOutput('diffPlot')
       )
    )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
    output$countryPlot <- renderPlot({
      dta %>%
@@ -48,6 +70,42 @@ server <- function(input, output) {
        geom_line(aes(x = `Survey Year`, y = Value, color = gender)) +
        facet_grid(`Characteristic Label`~Indicator)
    })
+
+   observe({
+     dem <- input$dem
+     updateSelectInput(session, "lab",
+                       choices = unique(dplyr::filter(diffDta, `Characteristic Category` == dem)$`Characteristic Label`))
+   })
+
+   output$diffPlot <- renderPlot({
+
+     interest <- diffDta %>%
+       dplyr::filter(
+         Indicator == input$indicator,
+         `Characteristic Category` == input$dem,
+         `Characteristic Label` == input$lab,
+         gender == input$gender
+       ) %>%
+       dplyr::arrange(desc(diff))
+
+     interestTop <- head(interest, 5)
+     interestBottom <- tail(interest, 5)
+
+     interest <- rbind(interestTop, interestBottom) %>%
+       dplyr::mutate(`Country Name` = factor(`Country Name`, `Country Name`))
+
+     ggplot(interest) +
+       geom_segment(aes(x = first,
+                        xend = last,
+                        y = `Country Name`,
+                        yend = `Country Name`,
+                        color = delta), size = 2, arrow = arrow(length = unit(0.03, "npc"))) +
+       labs(x = 'Difference in Acceptance',
+            y = 'Country',
+            title = paste0('Delta of Acceptance of Most First and Last Surveys'),
+            subtitle = paste0(input$dem, ' ', input$lab, ' ', input$gender))
+   })
+
 }
 
 # Run the application
--- R/dataCleaning.R
+++ R/dataCleaning.R
@@ -20,9 +20,54 @@ test <- y %>%
 
 write_csv(test, 'data/YearlyAttitudeTowardsWifeBeating.csv')
 
+#drill in specifically to country
 test %>%
   dplyr::filter(`Characteristic Category` == 'Marital status',
                 `Country Name` == "Senegal") %>%
   ggplot() +
   geom_line(aes(x = `Survey Year`, y = Value, color = gender)) +
   facet_grid(`Characteristic Label`~Indicator)
+
+diffYear <- test %>%
+  dplyr::arrange(`Survey Year`) %>%
+  tidyr::nest(data = c('Survey Year', 'Value')) %>%
+  dplyr::mutate(data = purrr::map(data, function(x){
+    dta <- tibble::tibble(
+      first = NA,
+      last = NA,
+      diff = NA
+    )
+    if(nrow(x) == 1) return(dta)
+    dta$first <- x$Value[1]
+    dta$last <- x$Value[nrow(x)]
+    dta$diff <- x$Value[nrow(x)] - x$Value[1]
+    return(dta)
+  })) %>%
+  tidyr::unnest() %>%
+  dplyr::filter(!is.na(diff)) %>%
+  dplyr::mutate(delta = ifelse(diff >= 0, 'positive', 'negative'))
+
+write_csv(diffYear, 'data/DiffYearAttitudeTowardsWifeBeating.csv')
+
+interest <- diffYear %>%
+  dplyr::filter(
+    Indicator == 'if she burns the food',
+    `Characteristic Category` == 'Education',
+    `Characteristic Label` == 'No education',
+    gender == 'Women'
+    ) %>%
+  dplyr::arrange(desc(diff))
+
+interestTop <- head(interest, 5)
+interestBottom <- tail(interest, 5)
+
+test2 <- rbind(interestTop, interestBottom) %>%
+  dplyr::mutate(`Country Name` = factor(`Country Name`, `Country Name`))
+
+ggplot(test2) +
+  geom_segment(aes(x = first,
+                   xend = last,
+                   y = `Country Name`,
+                   yend = `Country Name`,
+                   color = delta), size = 2, arrow = arrow(length = unit(0.03, "npc")))
+
